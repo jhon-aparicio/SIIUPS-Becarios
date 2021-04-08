7c93782299b67ea93f7063b96be1eadc2992d7c2
jhon-aparicio <79717032+jhon-aparicio@users.noreply.github.com>
2021-04-08 18:25:59 -0500

    08-04-21 6/25

--- Conexion.php
+++ Conexion.php
@@ -6,10 +6,17 @@ class  Db{
         
     }
     public static function conectar(){
+        //Usuario
         $user="udtlk68vhilazgjg";
+        //Contraseña
         $pass="aE4izB3O3rAvAB5CZMYh";
+        //Servidor (Url Donde esta nuestra base de datos)
         $server="bfoaidefugxz3dixvgsq-mysql.services.clever-cloud.com";
+        //Nombre de la Base de datos
         $database="bfoaidefugxz3dixvgsq";
+        //Conexión que se hace con uso de los elementos mencionados
+        //anteriormente, donde se retorna la conexión la cual nos sirve
+        //para traer y enviar los datos
         $conexion=new mysqli($server,$user,$pass,$database);
             return $conexion;
     }		
