890d3a2bb19bed0cee2047d3f8863ab31e623b91
jhon-aparicio <79717032+jhon-aparicio@users.noreply.github.com>
2021-04-08 20:23:55 -0500

    Update controlAdministrador.php
    Comentarios sobre algunos metodos

--- controlAdministrador.php
+++ controlAdministrador.php
@@ -25,16 +25,25 @@ class controlAdministrador {
         return $listarUsuarios;
     }
     public function mostrarD(){
+        //Conexion base de Datos
         $db= Db::conectar();
+        //Arreglo donde se guardaran nuestras dependencias
         $listarDependencia=[];
+        //Se realiza la consulta
         $select= mysqli_query($db,'SELECT * FROM Dependencia');
+        //Hacemos un while donde transformemos el resultado de la consulta en un objeto de tipo Dependenica
         while ($RQuery= mysqli_fetch_array($select)){
+            //Creamos el objeto Dpendenica
             $dependencia=new Dependencia();
+            //Hacemos un set donde le asiganmos los valores que estan dentro de nuestra base de datos
+            //Y hacen parte de nuestro objeto
             $dependencia->setId($RQuery['Id']);
             $dependencia->setNombre_Dependencia($RQuery['Nombre_Dependencia']);
             $dependencia->setNombre_Encargado($RQuery['Nombre_Encargado']);
+            //Añadimos nuestro objeto y lo agregamos a nuestro arreglo
             $listarDependencia[]=$dependencia;
         }
+        //Finalmente retornamos nuestro arreglo para que pueda ser usado y poder mostrar nuestras depnedencias
         return $listarDependencia;
     }
     public function numHorarios() {
@@ -44,15 +53,24 @@ class controlAdministrador {
         while($RQuery= mysqli_fetch_array($select)){$contador=$contador+1;}
         return $contador;
     }
+    //Este metodo no sirve para traer las dependencias de cada becario para complementar nuestra tabla becarios
     public function traerDepeU(){
+        //Establecemos conexión con la base de datos
         $db= Db::conectar();
+        //Creamos un arreglo 
         $dependencias=[];
+        //Creamos la consulta
         $consulta="SELECT D.Nombre_Dependencia as Nombre from Dependencia D INNER JOIN Becario B ON B.id_dependencia=D.Id;";
+        //Realizmaos la consulta
         $respuesta= mysqli_query($db, $consulta);
+        //Recorremos la consulta 
         while($RQuery= mysqli_fetch_array($respuesta)){
+            //Traemos el nombre de la dependencia y lo agregamos en nuestra variable
             $Nombre=$RQuery['Nombre'];
+            //Añadimos el nombre a nuestra arreglo
             $dependencias[]=$Nombre;
         }
+        //Finalmente retornamos el arreglo 
         return $dependencias;
     }
     public function traerBec($id) {
